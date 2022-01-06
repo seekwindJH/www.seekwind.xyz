8721a9a062cee759b385b51972c0e6caee1c1da0
seekwindJH <o1207629368@outlook.com>
2022-01-06 11:54:59 +0800

    SeekWind Commit

--- deploy.ps1
+++ deploy.ps1
@@ -1,23 +1,12 @@
+git add .
+git commit -m "SeekWind Commit"
+git push
+
 # Generate Hugo Static File
 hugo
 
 scp -r ./* root@seekwind.xyz:/root/App/www.seekwind.xyz/
 
-
-Set-Location public
-
-# Upload TO seekwind.xyz
-scp -r ./* root@seekwind.xyz:/data/nginx/html/
-
-# Upload TO github.com
-git init
-git add -A
-git commit -m "seekwind update"
-git branch -M main
-git remote add origin git@github.com:seekwindJH/seekwindJH.github.io.git
-git push -f origin main
-
-Set-Location ..
 
 IF (TEST-PATH .\public\) {
     Remove-Item -Force -Recurse .\public\
